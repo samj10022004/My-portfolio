3bc3feb152ef88fc3c8ce052be051f7d4d36d365
samj10022004 <97349711+samj10022004@users.noreply.github.com>
2024-05-25 02:16:51 +0530

    remove 2 components from the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ const App = () => {
         <Navbar/>
         <Hero/>
       </section>
-      <section id="Services"><Parallax type="services"/></section>
-      <section><Services/></section>
+      {/* <section id="Services"><Parallax type="services"/></section>
+      <section><Services/></section> */}
       <section id="Portfolio"><Parallax type="portfolio"/></section>
       <Portfolio/>
       <section id="Contact"><Contact/></section>
